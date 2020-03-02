eb527c3fda7ba108ea598ac1cfd9e97eb451e7c3
lichen <lichen@hydrolab.com>
2020-03-02 15:17:39 -0500

    waiting for geometric center

--- circle_Wu_20200302.m
+++ circle_Wu_20200302.m
@@ -1,13 +1,22 @@
 % drop_edge_Wu_20200216
 close all; 
 clc;
+currentHight = 1;
+currentRun = 2;
+prefix_1 = 'C:\Users\lab-admin\Desktop\Lichen_Wu\movies_processed\ndl14_hgt';
+prefix_6 = num2str(currentHight);
+prefix_7 ='_r';
+prefix_2 = num2str(currentRun);
+prefix_3 ='\ndl14_ht';
+prefix_8 =num2str(currentHight);
+prefix_9 ='_r';
+prefix_4 =num2str(currentRun);
+prefix_5= '_';
 
-
-prefix = 'C:\Users\lab-admin\Desktop\Lichen_Wu\movies_processed\ndl14_hgt4_r1\ndl14_ht4_r1_';
+prefix = strcat(prefix_1,prefix_6,prefix_7,prefix_2,prefix_3,prefix_8,prefix_9,prefix_4,prefix_5);
 
 ext = '.bmp';
 ext_out = '.txt';
-% OutputDir = 'C:\Users\lab-admin\Desktop\Lichen_Wu\images\Processed_20200107_ndl14_h4_r1\Circled_ndl14_h4_r1_';
 
 
 ref_index = input('Please enter the number of the reference image (ref_index =?):  ');
@@ -17,7 +26,7 @@ totalNumber = LastIm - FirstIm+1 ;
 
 ref_a = imread(strcat(prefix,num2str(ref_index, '%05g'),ext),'bmp'); 
 
-
+numCircledFailuer = 0;
 centersSave{totalNumber} = [];
 radiiSave{totalNumber} = [];
 for i = 0:1:totalNumber
@@ -40,7 +49,7 @@ for i = 0:1:totalNumber
     a1 = imadjust(a0, [0.01 a0_max], [0 1]); %for a better contrast
     BW = a0>max(max(a0))/5; %another way to convert into BW 
 
-    [centers,radii] = imfindcircles(BW,[40 65],'ObjectPolarity','bright');
+    [centers,radii] = imfindcircles(BW,[37 65],'ObjectPolarity','bright');
     centersSave{i+1} = centers;
     radiiSave{i+1} = radii;
     
@@ -49,10 +58,18 @@ for i = 0:1:totalNumber
     hold on
     viscircles(centers,radii);
     hold off
+    if(ii == LastIm + 1)
+        fprintf('Total %d images have been processed, %d have been circled', ...
+        totalNumber, totalNumber - numCircledFailuer);
+        break;
+    end
     if(size(radii) == 0)
+        numCircledFailuer = numCircledFailuer + 1;
         continue;
     end
-    if(size(radii)>1)
+    
+    siz=size(radii);
+    if(siz(1) > 1)
         disp('Detect more than one circles');
         break;
     end
