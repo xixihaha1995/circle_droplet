6f0a4edeb1a7eceb95b8f453caef40b7cb6124f0
xixihaha1995 <43649779+xixihaha1995@users.noreply.github.com>
2020-03-02 12:31:42 -0500

    Add files via upload

---
+++ circle_Wu_20200302.m
@@ -0,0 +1,67 @@
+% drop_edge_Wu_20200216
+close all; 
+clc;
+
+
+prefix = 'C:\Users\lab-admin\Desktop\Lichen_Wu\images\20200107_ndl14_h4_r1\ndl14_h4_r1_';
+ext = '.bmp';
+ext_out = '.txt';
+OutputDir = 'C:\Users\lab-admin\Desktop\Lichen_Wu\images\Processed_20200107_ndl14_h4_r1\Circled_ndl14_h4_r1_', ';
+thresh = 0.1;
+
+
+ref_index = input('Please enter the number of the reference image (ref_index =?):  ');
+FirstIm = ref_index + 1;
+LastIm = input('Please enter the last number of the image (LastIm =?):  ');
+totalNumber = LastIm - FirstIm+1 ;
+
+ref_a = imread(strcat(prefix,num2str(ref_index, '%05g'),ext),'bmp'); 
+
+
+centersSave{totalNumber} = [];
+radiiSave{totalNumber} = [];
+for i = 0:1:totalNumber
+ 
+    if i==0
+        ii = FirstIm;
+    else
+        ii = FirstIm + i;
+    end
+    
+    disp('The present image number: ');
+    disp(ii);
+    filename = strcat(prefix, num2str(ii, '%05g'),ext);
+    [a, map] = imread(filename);
+    a = imread(filename, 'bmp');
+
+    a0 =ref_a-a; %subtract the background
+    a0_max = double(max(max(a0)))/256.0;
+ 
+    a1 = imadjust(a0, [0.01 a0_max], [0 1]); %for a better contrast
+    BW = a0>max(max(a0))/5; %another way to convert into BW 
+%     [B, L]=bwboundaries(BW,'noholes');
+
+% %     edging
+%     a0 = imcrop(a,[20 300 2500 1000]);
+%     a1 = imadjust(a0, [0.2 0.7], [0 1]);
+%     bw = edge(a0,'canny', thresh);
+%     a2 = combine_image(a0, bw);
+%     aa = trace_image(a1,bw);
+%     outputFilename = strcat('Processed_ndl14_h4_r1_',num2str(ii, '%05g'),ext);
+%     saveas(gcf,[OutputDir,outputFilename]);
+% %     edging
+%   fitting circles
+    [centers,radii] = imfindcircles(BW,[45 58],'ObjectPolarity','dark');
+    centersSave{i+1} = centers;
+    radiiSave{i+1} = radii;
+    imshow(a)
+    h = viscircles(centers,radii);
+%     outputFilename = strcat('Circled_ndl14_h4_r1_',num2str(ii, '%05g'),ext);
+%     saveas(gcf,[OutputDir,outputFilename]);
+%   fitting circles
+    filename_out = strcat(OutputDir,num2str(ii, '%05g'),ext_out);
+    fid = fopen(filename_out,'w');
+    fprintf(fid, '%8.2f \t %8.2f \t %8.2f\n',[centers(1);centers(2); radii]); %relative to flat surface
+    fclose(fid);
+end
+
